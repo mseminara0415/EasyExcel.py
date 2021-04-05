5466034e9482570160c880a3b9a83254a23b48fa
mseminara <32893556+mseminara0415@users.noreply.github.com>
2021-04-05 13:25:25 -0700

    Update EasyExcel.py
    Included new method to add worksheets. Updated Docstrings.

--- EasyExcel.py
+++ EasyExcel.py
@@ -1,7 +1,3 @@
-"""
-EasyExcel is currently in-progress..
-"""
-
 import win32com.client as win32
 import os
 import re
@@ -11,19 +7,29 @@ import shutil
 
 class EasyExcel:
     """
-    EasyExcel acts as a wrapper for Pywin32, allowing for straight forward Excel editing/formatting.
+    Excel Editor is designed to reduce the clutter of formatting excel files with
+    Pywin32.
     """
+
+    class ExcelCharacterLimit(Exception):
+        def __init__(self, message):
+            self.message = message
+
+    class ExcludedCharacters(Exception):
+        def __init__(self, message):
+            self.message = message
 
     @staticmethod
     def initialize_excel(visible: bool, display_alerts: bool, screen_updating: bool, enable_events: bool):
         """
-        Initialize Pywin Excel application.
+        Iniitialize Excel Object. User can set the parameters to determine if the process is visible or not.
         :param visible:
         :param display_alerts:
         :param screen_updating:
         :param enable_events:
         :return:
         """
+
         # Set up Excel Workbook
         try:
             excel = win32.gencache.EnsureDispatch('Excel.Application')
@@ -51,24 +57,18 @@ class EasyExcel:
         self.filepath = file_path
         self.excel = self.initialize_excel(False, False, False, False)
         self.wb = self.excel.Workbooks.Open(self.filepath)
+        self.sheets = self.wb.Sheets
 
     @property
-    def sheets(self):
+    def sheet_list(self):
         """
-        Get all sheets within workbook.
+        Return a list of sheet names in workbook.
         :return:
         """
-        return [sheet for sheet in self.wb.Sheets]
+        return [sheet.Name for sheet in self.sheets]
 
     def color_scale(self, worksheet: str, cell_range_start=None,
-                    cell_range_end=None):
-        """
-        Add in color scale for selected range.
-        :param worksheet:
-        :param cell_range_start:
-        :param cell_range_end:
-        :return:
-        """
+                    cell_range_end=None, save=False):
 
         sheet = self.wb.Worksheets(worksheet)
 
@@ -81,8 +81,11 @@ class EasyExcel:
 
         working_range.FormatConditions.AddColorScale(ColorScaleType=3)
 
+        if save:
+            self.save()
+
     def merge_cells(self, worksheet, cell_range_start=None, cell_range_end=None,
-                    center_text=True):
+                    center_text=True, save=False):
         """
         Merge Excel Cell Range. Also Provides an option to center align.
         :return:
@@ -102,12 +105,16 @@ class EasyExcel:
         if center_text:
             sheet.Range(f"{cell_range_start}").HorizontalAlignment = -4108
 
-    def bold_cells(self, worksheet, cell_range_start=None, cell_range_end=None):
+        if save:
+            self.save()
+
+    def bold_cells(self, worksheet, cell_range_start=None, cell_range_end=None,save=False):
         """
-        Bold selected cell range.
+        Bold cell(s) within specified worksheet.
         :param worksheet:
         :param cell_range_start:
         :param cell_range_end:
+        :param save:
         :return:
         """
 
@@ -121,6 +128,47 @@ class EasyExcel:
             working_range = sheet.Range(f"{cell_range_start}")
 
         working_range.Font.Bold = True
+
+        if save:
+            self.save()
+
+
+    def add_sheet(self, name, save=False):
+        """
+        add worksheet to workbook.
+        :param name:
+        :param save:
+        :return:
+        """
+        wb = self.wb
+        ws = wb.Worksheets.Add()
+
+        excluded_characters = [r"\/?*,[,]:."]
+        matched_list = [characters in excluded_characters for characters in name]
+        string_name_includes_excluded = all(matched_list)
+
+        if string_name_includes_excluded:
+            raise EasyExcel.ExcludedCharacters(f"Worksheet names cannot include {excluded_characters}")
+
+        elif len(name) > 31:
+            raise EasyExcel.ExcelCharacterLimit("Please Limit name to 31 characters")
+
+        elif name in self.sheet_list:
+            number_of_sheets = len(self.sheet_list) + 1
+            ws.Name = name + str(number_of_sheets)
+
+        else:
+            ws.Name = name
+
+        if save:
+            self.save_work()
+
+    def save(self):
+        """
+
+        :return:
+        """
+        self.wb.Save()
 
     def close_workbook(self):
         """
